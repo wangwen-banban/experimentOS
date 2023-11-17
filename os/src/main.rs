338fb2d322e088e0fc2e2a936b0813ffc71f1ca7
wangwen <2040688016@qq.com>
2023-11-17 14:27:01 +0800

    姓名：王文，学号：21301106，操作系统第三次实验

--- os/src/main.rs
+++ os/src/main.rs
@@ -1,6 +1,40 @@
 #![no_std]
 #![no_main]
 #![feature(panic_info_message)]
+
+#[macro_use]
+mod console;
+mod lang_items;
+mod sbi;
+mod syscall;
+mod trap;
+mod batch;
+
+use core::arch::global_asm;
+
+global_asm!(include_str!("entry.asm"));
+global_asm!(include_str!("link_app.S"));
+
+fn clear_bss() {
+        extern "C" {
+                    fn sbss();
+                            fn ebss();
+                                }
+            (sbss as usize..ebss as usize).for_each(|a| unsafe { (a as *mut u8).write_volatile(0) });
+}
+
+#[no_mangle]
+pub fn rust_main() -> ! {
+        clear_bss();
+            println!("[Kernel] Hello, world!");
+                trap::init();
+                    batch::init();
+                        batch::run_next_app();
+}
+
+/*#![no_std]
+#![no_main]
+#![feature(panic_info_message)]
 #[macro_use]
 
 mod console;
@@ -49,7 +83,7 @@ pub fn rust_main() -> ! {
 
 
 
-
+*/
 
 /*#![no_std]
 #![no_main]
